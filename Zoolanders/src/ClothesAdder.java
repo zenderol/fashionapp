19f4de3dc22e2461f3a3fb5c8fefb5650334ce1f
zenderoleg@googlemail.com <zenderoleg@googlemail.com@26449a25-1d99-99c8-2b97-228075b157ce>
2014-01-06 22:56:20 +0000

    now it is possible to add 3d clothing

--- Zoolanders/src/ClothesAdder.java
+++ Zoolanders/src/ClothesAdder.java
@@ -3,6 +3,8 @@ import processing.core.PImage;
 import processing.core.PVector;
 import SimpleOpenNI.*;
 
+import saito.objloader.*;
+
 public class ClothesAdder extends PApplet { // extends Papplet because
 											// loadImage() needs it
 
@@ -19,8 +21,7 @@ public class ClothesAdder extends PApplet { // extends Papplet because
 			if (soni.isTrackingSkeleton(userIDs[i])) {
 				PVector head3d = new PVector();
 				PVector head2d = new PVector();
-				
-				
+
 				float confidence = soni.getJointPositionSkeleton(userIDs[i],
 						SimpleOpenNI.SKEL_HEAD, head3d);
 				soni.convertRealWorldToProjective(head3d, head2d);
@@ -31,36 +32,58 @@ public class ClothesAdder extends PApplet { // extends Papplet because
 				parent.imageMode(CENTER);
 				parent.image(face, head2d.x, head2d.y, d, d);
 
-//				text("head3d: " + head3d, 10, 20);
-//				text("head2d: " + head2d, 10, 40);
-//				text("d : " + d, 10, 60);
+				// System.out.println("Head added!");
+				// text("head3d: " + head3d, 10, 20);
+				// text("head2d: " + head2d, 10, 40);
+				// text("d : " + d, 10, 60);
 
 			}
 
 		}
 	}
-	
-	public void add3DShirt(int[] userIDs, String imagePath, SimpleOpenNI soni) {
-		face = loadImage(imagePath);
+
+	public void add3DShirt(int[] userIDs, String modelPath, SimpleOpenNI soni) {
+		OBJModel model = new OBJModel(parent, modelPath, "absolute", TRIANGLES);
+		model.translateToCenter();
+		model.scale(5);
+		parent.lights();
+		// model.draw();//added to parent because of OBJModel model = new
+		// OBJModel(parent,...
+
 		for (int i = 0; i < userIDs.length; i++) {
 			if (soni.isTrackingSkeleton(userIDs[i])) {
-				PVector head3d = new PVector();
-				PVector head2d = new PVector();
+
+//				PVector userposition = new PVector();
+//				soni.getCoM(userIDs[i], userposition);
+				
+				PVector torso3d = new PVector();
+				PVector torso2d = new PVector();
 				
 				
 				float confidence = soni.getJointPositionSkeleton(userIDs[i],
-						SimpleOpenNI.SKEL_HEAD, head3d);
-				soni.convertRealWorldToProjective(head3d, head2d);
+						SimpleOpenNI.SKEL_TORSO, torso3d);
+				soni.convertRealWorldToProjective(torso3d, torso2d);
 
-				float d = (float) (190e3 / head3d.z); // derive size of image,
-														// using distance
+		
+				
 
-				parent.imageMode(CENTER);
-				parent.image(face, head2d.x, head2d.y, d, d);
+				//PVector modelrotation = new PVector(userposition.x, userposition.y, userposition.z);
 
-//				text("head3d: " + head3d, 10, 20);
-//				text("head2d: " + head2d, 10, 40);
-//				text("d : " + d, 10, 60);
+				
+				parent.pushMatrix();
+
+				//parent.translate(userposition.x, userposition.y);
+				parent.translate(torso2d.x, torso2d.y);
+
+//				parent.rotateX(radians(torso3d.x));
+				 parent.rotateY(radians(torso3d.y));
+	          //  parent.rotateZ(radians(torso3d.z));
+				model.draw();
+
+				parent.popMatrix();
+
+				//parent.text("user position: " + userposition, 10, 20);
+				parent.text("user position: " + torso2d, 10, 20);
 
 			}
 
--- Zoolanders/src/SimpleOpenNI_User_eclipse.java
+++ Zoolanders/src/SimpleOpenNI_User_eclipse.java
@@ -1,22 +1,32 @@
 import SimpleOpenNI.*;
 import processing.core.*;
 
+
 public class SimpleOpenNI_User_eclipse extends PApplet {
 
 	public SimpleOpenNI  soni;
 	public PImage face;
 	public ClothesAdder clothesAdder = new ClothesAdder(this);
 
+	
 
 	public void setup() {
+		size(640, 480,P3D);
+		
 		soni = new SimpleOpenNI(this);
+
 		soni.enableDepth();
 		soni.enableRGB();
 		soni.enableUser();
-		size(soni.depthWidth(), soni.depthHeight());
-		//face = loadImage("..\\images\\face.png");
+		//size(soni.depthWidth(), soni.depthHeight()); //Original without obj loader
+		//size(soni.depthWidth(), soni.depthHeight(), P3D); //3d renderer
+
+		
+		
 		textSize(20);
 		fill(0,255,0);
+		
+		//frameRate(30); // fuer obj loader
 	}
 	
 	public void onNewUser(SimpleOpenNI context, int userId){
@@ -25,13 +35,16 @@ public class SimpleOpenNI_User_eclipse extends PApplet {
 	
 	public void draw() {
 		soni.update();
+		 background(0,0,0);
 		imageMode(CORNER);
 		image(soni.rgbImage(), 0, 0);
 		//image(soni.depthImage(), 0, 0);
 		int[] userIDs = soni.getUsers();
 		
+		
 		if(userIDs.length >0){
-		clothesAdder.add2DHead(userIDs, "..\\images\\face.png", soni);
+		//clothesAdder.add2DHead(userIDs, "..\\images\\face.png", soni);
+			clothesAdder.add3DShirt(userIDs, "..\\models\\black_tshirt_nike.obj", soni);
 		
 		
 		}

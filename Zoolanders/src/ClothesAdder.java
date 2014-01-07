e8c5bc9a696e7a147e57ec7c9e60802a7dce0939
zenderoleg@googlemail.com <zenderoleg@googlemail.com@26449a25-1d99-99c8-2b97-228075b157ce>
2014-01-07 20:59:44 +0000

    merge branch to trunk

---
+++ Zoolanders/src/ClothesAdder.java
@@ -0,0 +1,81 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PVector;
+import SimpleOpenNI.*;
+
+import saito.objloader.*;
+
+public class ClothesAdder extends PApplet { // extends Papplet because
+											// loadImage() needs it
+
+	PApplet parent; // The parent PApplet that we will render ourselves onto
+	public PImage face;
+
+	public ClothesAdder(PApplet p) {
+		parent = p;
+	}
+
+	public void add2DHead(int[] userIDs, String imagePath, SimpleOpenNI soni) {
+		face = loadImage(imagePath);
+		for (int i = 0; i < userIDs.length; i++) {
+			if (soni.isTrackingSkeleton(userIDs[i])) {
+				PVector head3d = new PVector();
+				PVector head2d = new PVector();
+
+				float confidence = soni.getJointPositionSkeleton(userIDs[i],
+						SimpleOpenNI.SKEL_HEAD, head3d);
+				soni.convertRealWorldToProjective(head3d, head2d);
+
+				float d = (float) (190e3 / head3d.z); // derive size of image,
+														// using distance
+
+				parent.imageMode(CENTER);
+				parent.image(face, head2d.x, head2d.y, d, d);
+
+				// System.out.println("Head added!");
+				 text("head3d: " + head3d, 10, 20);
+				// text("head2d: " + head2d, 10, 40);
+				// text("d : " + d, 10, 60);
+
+			}
+
+		}
+	}
+
+	public void add3DShirt(int[] userIDs, String modelPath, SimpleOpenNI soni) {
+		OBJModel model = new OBJModel(parent, modelPath, "absolute", TRIANGLES);
+		model.translateToCenter();
+		model.scale(5);
+		parent.lights();
+
+
+		for (int i = 0; i < userIDs.length; i++) {
+			if (soni.isTrackingSkeleton(userIDs[i])) {
+
+				PVector torso3d = new PVector();
+				PVector torso2d = new PVector();
+
+				float confidence = soni.getJointPositionSkeleton(userIDs[i],
+						SimpleOpenNI.SKEL_TORSO, torso3d);
+				soni.convertRealWorldToProjective(torso3d, torso2d);
+
+				/* Movement of the 3d Model according to the user */
+				parent.pushMatrix();
+
+				// parent.translate(userposition.x, userposition.y);
+				parent.translate(torso2d.x, torso2d.y);
+				// parent.rotateX(radians(torso3d.x));
+				// parent.rotateY(radians(torso3d.y));
+				// parent.rotateZ(radians(torso3d.z));
+				model.draw();
+
+				parent.popMatrix();
+
+				// parent.text("user position: " + userposition, 10, 20);
+				parent.text("user position: " + torso2d, 10, 20);
+
+			}
+
+		}
+	}
+}
